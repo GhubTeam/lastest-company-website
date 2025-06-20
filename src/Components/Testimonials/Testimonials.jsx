2e6ec4454d8319f5491943658514a68b05079970
ojingwaDaniel <ojingwadanny@gmail.com>
2025-06-20 09:48:19 +0100

    made some extra changes

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,17 +1,16 @@
-import  { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { BsArrowRightCircleFill, BsArrowLeftCircleFill } from "react-icons/bs";
-import TestimonialData from "./TestimonialData"; 
-import "./Testimonials.css"
+import TestimonialData from "./TestimonialData";
+import "./Testimonials.css";
 
 const TestimonialCarousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % TestimonialData.length);
-    }, 5000); 
-    return () => clearInterval(interval); 
+    }, 5000);
+    return () => clearInterval(interval);
   }, []);
 
   const nextTestimonial = () => {
@@ -26,22 +25,30 @@ const TestimonialCarousel = () => {
   };
 
   return (
-    <div className = "Testimonials">
+    <div className="Testimonials">
       <p className="testimony-subtitle">
-        <span className = "white-text bigger-text"> Real Stories, Real Impact:</span> Hear from Our Satisfied Clients
+        <span className="white-text bigger-text">
+          Real Stories, Real Impact:
+        </span>
+        Hear from Our Satisfied Clients
       </p>
       <div className="testimonial-section">
         <div className="testimonial-carousel">
           <div className="testimonial-card">
             <img
+              key={TestimonialData[currentIndex].img}
               src={TestimonialData[currentIndex].img}
               alt={TestimonialData[currentIndex].name}
               className="testimonial-image"
             />
             <div className="testimonial-content">
               <h3>{TestimonialData[currentIndex].name}</h3>
-              <p className = "location">{TestimonialData[currentIndex].location}</p>
-              <p className="testimonial-text">{TestimonialData[currentIndex].Testimony}</p>
+              <p className="location">
+                {TestimonialData[currentIndex].location}
+              </p>
+              <p className="testimonial-text">
+                {TestimonialData[currentIndex].Testimony}
+              </p>
             </div>
           </div>
         </div>
